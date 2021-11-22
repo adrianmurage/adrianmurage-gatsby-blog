31fd50d9baf26dadaab4ee5e19f75a08727f7bc8
adrianmurage <adrianmurage21@gmail.com>
2021-11-22 22:33:45 +0300

    replace profile picture and bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/profile-pic.JPG"
         width={50}
         height={50}
         quality={95}
@@ -45,10 +45,13 @@ const Bio = () => {
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
+          {` `} <a href="https://twitter.com/levelsio">@levelsio.</a>
+          <br></br>
+          <br></br>
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+            @adrianmurage
+          </a>{" "}
+          on twitter
         </p>
       )}
     </div>
